28a8e50ab7c2c906a132a0343a65bed5b683a3ce
Documenter.jl <documenter@juliadocs.github.io>
2025-08-15 18:44:11 +0000

    build based on f0913cc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MTCR","page":"Home","title":"MTCR","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MTCR.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
